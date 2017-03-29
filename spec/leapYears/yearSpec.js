303df5e3f49bb417f3114e23522ce5d7c424e6a5
Julien Marciliac <julien.marciliac@gmail.com>
2017-03-29 22:35:53 +0200

    leapYear 7

--- spec/leapYears/yearSpec.js
+++ spec/leapYears/yearSpec.js
@@ -17,5 +17,20 @@ describe("LeapYear", function() {
         expect(year.isLeap()).toEqual(false);
     });
 
+    it("should return false . A leap year is divisible by 4, but is not otherwise divisible by 100 unless it is also divisible by 400", function() {
+        var year = new Year(1900);
+        expect(year.isLeap()).toEqual(false);
+    });
+
+    it("should return true if it s a leap year ", function() {
+        var year = new Year(1996);
+        expect(year.isLeap()).toEqual(true);
+    });
+
+    it("should return true if it s a atypical leap year", function() {
+        var year = new Year(2000);
+        expect(year.isLeap()).toEqual(true);
+    });
+
 
 });
