e0fbbc54ed04be588a2b6a3829579a01a88bb961
Julien Marciliac <julien.marciliac@gmail.com>
2017-04-05 23:22:52 +0200

    stringCalc Spec it should returns the argument if the argument is a number alone

--- spec/stringCalculator/stringCalculatorSpec.js
+++ spec/stringCalculator/stringCalculatorSpec.js
@@ -2,9 +2,12 @@ describe("StringCalculator", function() {
 
     var Calc = require('../../lib/stringCalculator/stringCalculator.js');
 
+    it("should returns 0 if the argument is an empty string", function() {
+        expect(Calc.add('')).toEqual(0);
+    });
 
-    it("returns 0 if the input is an empty string", function() {
-        expect(Calc.Add('')).toEqual(0);
+    it("should returns the argument if the argument is a number alone", function() {
+        expect(Calc.add('1') ).toEqual(1);
     })
 
 });
